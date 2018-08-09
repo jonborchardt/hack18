a11d85c5c903fe52e730865d58f98d0a340c6fc7
Candace Ross <candacelax@gmail.com>
2018-08-09 09:32:24 -0700

    filtering authors with single letter first names

--- gender_analysis/get_freq.py
+++ gender_analysis/get_freq.py
@@ -16,18 +16,17 @@ options = { 'male'          : 'male',
             'customDict'    : 'legenderary/custom.txt',
             'bingAPIKey'    : 'ABC123478ZML'
           }
-gender = leGenderary(options) 
+gender = leGenderary(options)
 
 def parse_args():
     parser = argparse.ArgumentParser('Get top-k most frequent first author names')
     parser.add_argument('--k', type=int, default=100)
-    parser.add_argument('--s2', type=str, default='../data', help='pattern for s2 files')
+    parser.add_argument('--s2', type=str, default='../data/cleaned', help='path to s2 files')
     parser.add_argument('--out', type=str, default='../data/top_k.csv')
     return parser.parse_args()
 
 def load_s2_data(s2_path):
-    fpaths = filter(lambda fp: re.match('s2-corpus-[0-9]+$', fp), os.listdir(s2_path)) # s2 corpus files
-
+    fpaths = filter(lambda fp: re.match('s2-corpus-[0-9]+', fp), os.listdir(s2_path)) # s2 corpus files
     num_missing=0
     first_authors = []
     for fp in sorted(fpaths):
@@ -40,7 +39,7 @@ def load_s2_data(s2_path):
                     num_missing+=1
                     continue
 
-                full_name = as_json['authors'][0]['name']
+                full_name = as_json['authors'][0]#['name']
                 first_authors.append(get_first_name(full_name))
     print '{} missing author'.format(num_missing)
     return first_authors
@@ -61,11 +60,12 @@ if __name__ == '__main__':
     args = parse_args()
 
     first_names = load_s2_data(args.s2)
-    freq_counts = sorted(Counter(first_names).items(),
+    no_single_letters = filter(lambda x: len(x) > 1, first_names)
+    
+    freq_counts = sorted(Counter(no_single_letters).items(),
                          key=lambda x: x[1],
                          reverse=True)
     top_k = freq_counts[0:args.k]
-    print top_k
 
     with open(args.out, 'w') as f:
         f.write('Name,Count,Gabi,Candace,Jonathan,Matt\n')
