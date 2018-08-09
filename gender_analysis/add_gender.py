0b58cba3ce575af69f043b57685e41e659ed188e
Gabi Stanovsky <gabis@julianm.local>
2018-08-09 15:34:36 -0700

    better gender prediction

--- gender_analysis/add_gender.py
+++ gender_analysis/add_gender.py
@@ -46,6 +46,13 @@ def get_gender(full_name):
     return gender.determineGender(full_name)
 
 
+def lazy_paper_reader(fn):
+    """
+    Read jsonl file lazyly
+    """
+    for line in open(inp_fn):
+        yield(json.loads(line.strip()))
+
 if __name__ == "__main__":
 
     # Parse command line arguments
@@ -60,30 +67,20 @@ if __name__ == "__main__":
 
 
     # Load input file
-    logging.info("Reading {}".format(inp_fn))
-    paper_jsons = [json.loads(line.strip())
-                   for line
-                   in open(inp_fn)]
-
-    logging.info("Read {} paper records".format(len(paper_jsons)))
-
     logging.info("Adding gender...")
     authors_count = 0
-    for paper in tqdm(paper_jsons):
-        for author in paper["authors"]:
-            authors_count += 1
-            full_name = author['name']
-            first_name = gender.determineFirstName(full_name.split())
-            author['first_name'] = first_name
-            author['gender'] = gender.determineGender(full_name)
-            #TODO: gender.determineGender is supposed to be better, but throws a decoding error?
+    with open(out_fn, 'w') as fout:
+        for paper in tqdm(lazy_paper_reader(inp_fn)):
+            for author in paper["authors"]:
+                authors_count += 1
+                full_name = author['name']
+                first_name = gender.determineFirstName(full_name.split())
+                author['first_name'] = first_name
+                author['gender'] = gender.determineGender(full_name)
+                #TODO: gender.determineGender is supposed to be better, but throws a decoding error?
+            fout.write('{}\n'.format(json.dumps(paper)))
 
 
     logging.info("Added gender to {} authors".format(authors_count))
 
-    # Write to file
-    logging.info("Writing output to {}".format(out_fn))
-    with open(out_fn, 'w') as fout:
-        fout.write('\n'.join([json.dumps(paper)
-                              for paper in paper_jsons]))
     logging.info("DONE")
