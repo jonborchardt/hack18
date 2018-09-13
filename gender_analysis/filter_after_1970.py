9f73e9ddbbeddbbfcdb9475dc1c3f0225a66dc08
gabrielStanovsky <gabriel.satanovsky@gmail.com>
2018-09-13 20:37:43 +0000

    wip

---
+++ gender_analysis/filter_after_1970.py
@@ -0,0 +1,58 @@
+""" Usage:
+    <file-name> --in=INPUT_FILE --out=OUTPUT_FILE [--debug]
+
+* Filter only articles published after 1970 to the output file.
+
+"""
+import sys
+# sys.setdefaultencoding() does not exist, here!
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
+# External imports
+import logging
+import pdb
+from pprint import pprint
+from pprint import pformat
+from docopt import docopt
+from collections import defaultdict
+from operator import itemgetter
+import json
+from tqdm import tqdm
+
+# Local imports
+from add_gender import lazy_paper_reader
+#=-----
+
+from collections import namedtuple
+
+
+def normalize_source(source):
+    """
+    Normalize a paper source.
+    """
+    return source.lower().rstrip().lstrip()
+
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    args = docopt(__doc__)
+    inp_fn = args["--in"]
+    out_fn = args["--out"]
+    debug = args["--debug"]
+    if debug:
+        logging.basicConfig(level = logging.DEBUG)
+    else:
+        logging.basicConfig(level = logging.INFO)
+
+    cnt = 0
+    with open(out_fn, 'w') as fout:
+        for paper in tqdm(lazy_paper_reader(inp_fn)):
+            if paper["year"] >= 1970:
+                cnt +=1
+                fout.write("{}\n".format(json.dumps(paper)))
+
+
+    logging.info("Wrote {} papers to {}".format(cnt, out_fn))
+    logging.info("DONE")
---
+++ gender_analysis/out_citations_by_gender.py
@@ -0,0 +1,77 @@
+""" Usage:
+    <file-name> --in=INPUT_FILE --out=OUTPUT_FILE [--debug]
+
+Answer:
+
+1. Is the number (proportion) of female authors increasing?
+2. Is the number (proportion) of female first authors increasing?
+6. Bechedel
+"""
+import sys
+# sys.setdefaultencoding() does not exist, here!
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
+# External imports
+import logging
+import pdb
+from pprint import pprint
+from pprint import pformat
+from docopt import docopt
+from collections import defaultdict
+from operator import itemgetter
+import json
+from tqdm import tqdm
+
+# Local imports
+from add_gender import lazy_paper_reader
+#=-----
+
+from collections import namedtuple
+
+
+
+year_record = namedtuple("YearRecord",
+                         ["out_citation_by_gender"])
+
+
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    args = docopt(__doc__)
+    inp_fn = args["--in"]
+    out_fn = args["--out"]
+    debug = args["--debug"]
+    if debug:
+        logging.basicConfig(level = logging.DEBUG)
+    else:
+        logging.basicConfig(level = logging.INFO)
+
+
+    out_citation_by_year = defaultdict(lambda: defaultdict(lambda: 0))
+
+
+    for paper in tqdm(lazy_paper_reader(inp_fn)):
+        cur_year_record = out_citation_by_year[paper["year"]]
+
+        # all authors stats
+        for cur_author in paper["authors"]:
+            cur_year_record[cur_author["gender"]] += len(paper["outCitations"])
+
+    logging.info("Writing to {}".format(out_fn))
+
+    header = ["year", "male-out-citations", "female-out-citations", "unknown-out-citations"]
+    with open(out_fn, 'w') as fout:
+        fout.write("{}\n{}".format(','.join(header),
+                                   "\n".join([','.join(map(str,
+                                                           [year,
+                                                            year_record["male"],
+                                                            year_record["female"],
+                                                            year_record["unknown"]]))
+                                              for (year, year_record)
+                                              in sorted(dict(out_citation_by_year).iteritems())])))
+
+
+
+    logging.info("DONE")
