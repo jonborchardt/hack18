b3c02ba8b10068791da656c4ab72d3e96a186166
Gabi Stanovsky <gabis@julianm.local>
2018-09-13 17:40:15 -0700

    adding stats

--- gender_analysis/qnd.py
+++ gender_analysis/qnd.py
@@ -2,5 +2,10 @@ from sqlite_manager import Sqlite_Database
 import sys
 
 with Sqlite_Database(sys.argv[1], read_only = True) as db:
-    unique_venues = db.conn.execute("SELECT count(distinct venue) FROM papers;").fetchall()
+    print("counting venues before 2000...")
+    unique_venues = db.conn.execute("SELECT count(distinct venue) FROM papers WHERE year <= 2000 ;").fetchall()
+    print("#unique venues = {}".format(unique_venues))
+
+    print("counting venues after 2000...")
+    unique_venues = db.conn.execute("SELECT count(distinct venue) FROM papers WHERE year > 2000 ;").fetchall()
     print("#unique venues = {}".format(unique_venues))
