0cbf3ccf3c79354332e69025065a98e270a965b3
Gabi Stanovsky <gabis@julianm.local>
2018-09-19 17:56:11 -0700

    adding stats

--- gender_analysis/genderize_name_list.py
+++ gender_analysis/genderize_name_list.py
@@ -1,5 +1,5 @@
 """ Usage:
-    <file-name> --in=INPUT_FILE --out=OUTPUT_FILE [--debug]
+    <file-name> --in=INPUT_FILE --out=OUTPUT_FILE [--api=API_KEY] [--debug]
 """
 # Set default encoding to utf8
 import sys
@@ -12,8 +12,9 @@ import logging
 from pprint import pprint
 from pprint import pformat
 from docopt import docopt
-from genderize import Genderize
+from genderize import Genderize, GenderizeException
 from tqdm import tqdm
+from unidecode import unidecode
 
 # Local imports
 
@@ -24,12 +25,20 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
+
+def sanitize(first_name):
+    """
+    Clean a first name.
+    """
+    return unidecode(first_name.decode()).capitalize()
+
 if __name__ == "__main__":
 
     # Parse command line arguments
     args = docopt(__doc__)
     inp_fn = args["--in"]
     out_fn = args["--out"]
+    api_key = args["--api"]
     debug = args["--debug"]
     if debug:
         logging.basicConfig(level = logging.DEBUG)
@@ -38,17 +47,23 @@ if __name__ == "__main__":
 
 
     # Extract first names
-    first_names = ["".join(line.strip().split(",")[: -1])
+    first_names = [sanitize("".join(line.strip().split(",")[: -1]))
                    for line in open(inp_fn)]
 
-    g = Genderize()
+    g = Genderize(api_key = api_key) if api_key is not None \
+                            else Genderize()
     cnt = 0
     with open(out_fn, 'w') as fout:
         for name_chunks in tqdm(chunks(first_names, 10), total = len(first_names) / 10):
-            cnt += len(name_chunks)
-            data = g.get(name_chunks)
-            fout.write('\n'.join(["{},{}".format(name, ret['gender'])
-                                  for name, ret in zip(name_chunks, data)]) + '\n')
+            try:
+                data = g.get(name_chunks)
+                fout.write('\n'.join(["{},{}".format(name, ret['gender'])
+                                      for name, ret in zip(name_chunks, data)]) + '\n')
+                cnt += len(name_chunks)
 
-    logging.debug("Wrote {} names".format(cnt))
+            except GenderizeException as e:
+                logging.error("Couldn't genderize: {}".format(name_chunks))
+                logging.error(e)
+
+    logging.info("Wrote {} names".format(cnt))
     logging.info("DONE")
